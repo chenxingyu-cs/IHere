6318a4e3c7fa4c8b75fc5f0e796d308cb166cb9c
Xingyu Chen <chenxingyu.cs@gmail.com>
2016-04-03 14:46:44 -0700

    modify the ui

--- app/src/main/java/cmu/sv/xingyuchen/ihere/MainPageActivity.java
+++ app/src/main/java/cmu/sv/xingyuchen/ihere/MainPageActivity.java
@@ -84,7 +84,7 @@ public class MainPageActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+//        } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
