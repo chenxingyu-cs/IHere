702f957310f111bb7c76027653f40be5991b106c
zwang1 <wangzhengyicmu@gmail.com>
2016-04-03 18:46:57 -0700

    change icon

--- app/src/main/java/cmu/sv/flubber/ihere/dummy/HomeActivity.java
+++ app/src/main/java/cmu/sv/flubber/ihere/dummy/HomeActivity.java
@@ -62,20 +62,7 @@ public class HomeActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -98,9 +85,7 @@ public class HomeActivity extends AppCompatActivity
             Intent intent = new Intent(this, ItemListActivity.class);
             startActivity(intent);
 
-        } else if (id == R.id.nav_profile) {
-            Intent intent = new Intent(this, ProfileActivity.class);
-            startActivity(intent);
+        } else if (id == R.id.nav_logout) {
 
 
         } else if (id == R.id.nav_share) {
--- app/src/main/java/cmu/sv/flubber/ihere/dummy/ProfileActivity.java
+++
@@ -1,7 +0,0 @@
-package cmu.sv.flubber.ihere.dummy;
-
-/**
- * Created by zhengyiwang on 4/3/16.
- */
-public class ProfileActivity {
-}
