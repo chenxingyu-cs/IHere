a033729723cd5b0e867e13e33ae03ac1accc50b5
zwang1 <wangzhengyicmu@gmail.com>
2016-04-13 11:42:50 -0700

    localconnector need to implement

--- app/src/main/java/cmu/sv/flubber/ihere/dbLayout/DBLocalConnector.java
+++ app/src/main/java/cmu/sv/flubber/ihere/dbLayout/DBLocalConnector.java
@@ -1,23 +1,111 @@
 package cmu.sv.flubber.ihere.dbLayout;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
+
 /**
  * Created by zhengyiwang on 4/13/16.
  */
-public interface DBLocalConnector {
-    /*
-    This Connector to used to connect to local sqlite db on device,
-    which only save user name and email for showing
-    */
+public class DBLocalConnector implements DBLocalConnectorInterface {
+        // tag
+        private static final String TAG = "DatabaseConnector";
 
-    String getUserEmail();
+        // database info
+        public static final String DATABASE_NAME = "StudentRecord";
+        private SQLiteDatabase database; // database object
+        private DatabaseOpenHelper databaseOpenHelper; // database helper
 
-    String getUserName();
+        // table names
+        public static final String STU_QUIZ_TABLE = "studentQuizes";
 
-    void setUserName(String name);
+        // studentQuizes columns
+        public static final String KEY_STU_ID = "stuId";
+        public static final String KEY_Q1_SCORE = "q1";
+        public static final String KEY_Q2_SCORE = "q2";
+        public static final String KEY_Q3_SCORE = "q3";
+        public static final String KEY_Q4_SCORE = "q4";
+        public static final String KEY_Q5_SCORE = "q5";
 
-    void setUserEmail(String email);
+        // public constructor for DatabaseConnector
+        public DBLocalConnector(Context context)
+        {
+            // create a new DatabaseOpenHelper
+            databaseOpenHelper =
+                    new DatabaseOpenHelper(context, DATABASE_NAME, null, 1);
+        } // end DatabaseConnector constructor
 
-    void updateuserEmail(String email);
+        // open the database connection
+        public void open() throws SQLException
+        {
+            // create or open a database for reading/writing
+            database = databaseOpenHelper.getWritableDatabase();
+        } // end method open
+
+        // close the database connection
+        public void close()
+        {
+            if (database != null)
+                database.close(); // close the database connection
+        } // end method close
 
 
+        private class DatabaseOpenHelper extends SQLiteOpenHelper
+        {
+            // public constructor
+            public DatabaseOpenHelper(Context context, String name,
+                                      SQLiteDatabase.CursorFactory factory, int version)
+            {
+                super(context, name, factory, version);
+            } // end DatabaseOpenHelper constructor
+
+            // creates the contacts table when the database is created
+            @Override
+            public void onCreate(SQLiteDatabase db)
+            {
+                // query to create a new table named contacts
+                String createQuery = String.format("CREATE TABLE %s " +
+                                "(_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                " %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER);",
+                        STU_QUIZ_TABLE, KEY_STU_ID, KEY_Q1_SCORE, KEY_Q2_SCORE,
+                        KEY_Q3_SCORE, KEY_Q4_SCORE, KEY_Q5_SCORE);
+
+                db.execSQL(createQuery); // execute the query
+            } // end method onCreate
+
+            @Override
+            public void onUpgrade(SQLiteDatabase db, int oldVersion,
+                                  int newVersion)
+            {
+            } // end method onUpgrade
+        } // end class DatabaseOpenHelper
+
+
+
+    @Override
+    public String getUserEmail() {
+        return null;
+    }
+
+    @Override
+    public String getUserName() {
+        return null;
+    }
+
+    @Override
+    public void setUserName(String name) {
+
+    }
+
+    @Override
+    public void setUserEmail(String email) {
+
+    }
+
+    @Override
+    public void updateuserEmail(String email) {
+
+    }
 }
---
+++ app/src/main/java/cmu/sv/flubber/ihere/dbLayout/DBLocalConnectorInterface.java
@@ -0,0 +1,25 @@
+package cmu.sv.flubber.ihere.dbLayout;
+
+/**
+ * Created by zhengyiwang on 4/13/16.
+ */
+public interface DBLocalConnectorInterface {
+    /*
+    This Connector to used to connect to local sqlite db on device,
+    which only save user name and email for showing
+    */
+
+    String getUserEmail();
+
+    String getUserName();
+
+    void setUserName(String name);
+
+    void setUserEmail(String email);
+
+    void updateuserEmail(String email);
+
+
+}
+
+
