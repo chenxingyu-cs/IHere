e5eed363242b4f0521ddd36f087016323b7d2930
Kstain <kstainwwy@gmail.com>
2016-05-02 16:57:41 -0700

    distance update

--- app/src/main/java/cmu/sv/flubber/ihere/ui/ItagDetailActivity.java
+++ app/src/main/java/cmu/sv/flubber/ihere/ui/ItagDetailActivity.java
@@ -263,20 +263,14 @@ public class ItagDetailActivity extends HomeActivity {
     }
 
     private double gps2m(double lat_b, double lng_b) {
-        double lat_a = latitude;
-        double lng_a = longitude;
-        double pk = (float) (180/3.14169);
+        Location myLocation = new Location("");//provider name is unecessary
+        myLocation.setLatitude(latitude);//your coords of course
+        myLocation.setLongitude(longitude);
 
-        double a1 = lat_a / pk;
-        double a2 = lng_a / pk;
-        double b1 = lat_b / pk;
-        double b2 = lng_b / pk;
+        Location targetLocation = new Location("");//provider name is unecessary
+        targetLocation.setLatitude(lat_b);//your coords of course
+        targetLocation.setLongitude(lng_b);
 
-        double t1 = Math.cos(a1)*Math.cos(a2)*Math.cos(b1)*Math.cos(b2);
-        double t2 = Math.cos(a1)*Math.sin(a2)*Math.cos(b1)*Math.sin(b2);
-        double t3 = Math.sin(a1)* Math.sin(b1);
-        double tt = Math.acos(t1 + t2 + t3);
-
-        return 6366000*tt;
+        return myLocation.distanceTo(targetLocation);
     }
 }
